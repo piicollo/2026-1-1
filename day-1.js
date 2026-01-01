63510328f3ba16ad83e460ea091bcc9a5d596017
piicollo <kapilchharahangrai@gmail.com>
2026-01-01 21:53:29 +0545

    JS Number type

---
+++ day-1.js
@@ -0,0 +1,97 @@
+// Number Types 
+// The number data type represents a numeric value: 
+// let wholeNumber = 20;
+// let decimalNumber = 4.5;
+// let negativeNumber = -7;
+// console.log(wholeNumber);
+// console.log(decimalNumber);
+// console.log(negativeNumber);
+
+// Integers are whole numbers without any fractional or decimal part: 
+// let positiveInteger =11;
+// let negativeInteger = -2;
+// let zero = 0;
+// console.log(positiveInteger);
+// console.log(negativeInteger);
+// console.log(zero);
+
+// Floating point numbers with decimal points:
+
+// let floatingPointNumber = 4.5;
+// let anotherFloat = 89.99;
+// let oneMoreFloat = 16.22;
+// console.log(typeof floatingPointNumber);
+// console.log(typeof anotherFloat);
+// console.log(typeof oneMoreFloat);
+// console.log(floatingPointNumber);
+
+// Numbers that are beyond the maximum limit with infinity: 
+// const infiniteNumber = 1 / 0;
+// console.log(infiniteNumber); // Infinity
+// console.log(typeof infiniteNumber); // number
+
+// Not a Number: 
+// let firstNmber = 'Hell' / 2;
+// console.log(firstNmber); // NaN
+// console.log(typeof firstNmber); // number
+
+// Arithemetic Operators:
+// The addition operator (+) Plus Sign:
+
+// const num1 =10;
+// const num2 = 5;
+// const num3 = 15;
+
+// const result1 = num1 + num2;
+// const result2 = num2 + num1;
+// const result3 = num2 + num1 + num3;
+
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+
+// The subtraction operator (-) minus sign: 
+
+// const num4 = 55;
+// const num5 = 20;
+// console.log("Result: ", num5 - num4);
+// let result4 = num4 - num5;
+// console.log(`The result of these two numbers ${num4} and ${num5}: ${result4}`);
+
+// The multiplication operator (*) an asterisk sign :
+
+// let num6 = 2;
+// let num7 = 5;
+// const multi = num6 * num7;
+// console.log(multi);
+// num7 = 50;
+// let result = num6 * num7;
+// console.log(result);
+
+// The division operator (/) a slash sign :
+
+// let num8 = 20;
+// let num9 = 10;
+// let num10 = 5;
+// console.log("Result: ", num8 / num10);
+// console.log(`The result of ${num9} divide by ${num10} is, ${num9 / num10}`);
+// let num11 = 2;
+// let result = num10 / num11;
+// console.log(result);
+
+// Result of divide by zero(0):
+
+// let newNumber = 20;
+// let result5 = newNumber / 0;
+// console.log(result5); // Infinity
+
+// The exponentiation operator, (**) a double aserisk :
+// let numb = 2;
+// let numb2 = 3;
+// let exponent = numb ** numb2;
+// console.log("Result: ", exponent);
+
+// const mixResult = 10 + 2 * 3 - 8 / 2;
+// console.log("Result: ", mixResult);
+
+// Result of doing calculation with numbers and strings: 
