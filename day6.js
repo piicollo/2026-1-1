d4714d005cdece764d979b98939fde0b8794a0b2
piicollo <kapilchharahangrai@gmail.com>
2026-01-10 18:28:12 +0545

    Math methods

---
+++ day6.js
@@ -0,0 +1,50 @@
+// Build a Mathbot:
+
+const botName = "MathBot";
+const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object.`;
+console.log(greeting);
+
+console.log("The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.");
+
+const randomNum = Math.random();
+console.log(randomNum);
+
+console.log("Now, generate a random number between two values: ");
+const min = 1;
+const max = 100;
+
+const randomNum1 = Math.random() * (max - min) + min;
+console.log(randomNum1);
+
+console.log("The Math.floor() method rounds the value down to the nearest whole integer.");
+
+const numRoundedDown = 6.7;
+console.log(Math.floor(numRoundedDown));
+
+console.log("Now, generate a random integer between two values.");
+
+const randomInt = Math.floor(Math.random() * (max - min) + min);
+console.log(randomInt);
+
+console.log("The Math.ceil() method rounds the value up to the nearest whole integer.");
+
+const numRoundedUp = Math.ceil(3.2);
+console.log(numRoundedUp);
+
+console.log("The Math.round() method rounds the value to the nearest whole integer.");
+
+const numRounded  = Math.round(2.7);
+console.log(numRounded); // 3
+
+const numRounded1 = Math.round(11.2);
+console.log(numRounded1); // 11
+
+console.log("The Math.max() and Math.min() methods are used to get the maximum and minimum number from a range.");
+
+const maxNum = Math.max(3, 125, 55, 24);
+console.log(maxNum); // 125
+
+const minNum = Math.min(6, 90, 14, 90, 2);
+console.log(minNum); // 2
+
+console.log("It was fun learning about the different Math methods with you!");
