ce62d9fc8893772fd3a9531e674ea8b840490df4
piicollo <kapilchharahangrai@gmail.com>
2026-01-29 08:25:56 +0545

    switch statement examples

---
+++ day15.js
@@ -0,0 +1,46 @@
+// switch statement 
+
+let gender = 4;
+
+switch (gender) {
+    case 1:
+        console.log(`Gender: Male`);
+        break;
+    case 2:
+        console.log(`Gender: Female`);
+        break;
+    default:
+        console.log(`Gender: Others`);
+}
+
+let snacks = 'burger';
+
+switch (snacks) {
+    case 'momo':
+        console.log(`Nepali dumplings`); 
+        break;
+    case 'Thakali':
+        console.log(`Nepali cuisine`);
+        
+    default:
+        console.log(`Out of Order`);
+        
+}
+
+let book = 'computer science';
+
+switch (book) {
+    case 'computer science' :
+        console.log('Book about computers');
+        break;
+    case 'Nepali' :
+        console.log('Book about Nepali language');
+        break;
+    case 'math':
+        console.log('Book about calculation');
+        break;
+    default:
+        console.log('Out of context');
+                    
+        
+}
