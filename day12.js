acc2f62db3de7c1f09c03445860ee7b097e418ed
piicollo <kapilchharahangrai@gmail.com>
2026-01-29 08:24:59 +0545

    null and undefined data types modified

--- day12.js
+++ day12.js
@@ -1,25 +1,23 @@
 // Null and Undefined Data Types:
-// console.log(null == undefined); // true
+console.log(null == undefined); // true
 
-// console.log(null === undefined); // false
-// console.log(null == 0); // false
-// console.log(null == ""); // false
-// console.log(undefined == 0); // false 
-// console.log(undefined == ''); // false
+console.log(null === undefined); // false
+console.log(null == 0); // false
+console.log(null == ""); // false
+console.log(undefined == 0); // false 
+console.log(undefined == ''); // false
 
 // null behavior in comparisons:
 
-// console.log(null > 0); // false
-// console.log(null == 0); // false
-// console.log(null >= 0); // true
-// console.log(null < 0); // false
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+console.log(null < 0); // false
 
 
 // undefined comparison in NaN
-// console.log(undefined > 0); // false 
-// console.log(undefined < 0); // false
-// console.log(undefined == 0); // false
-// console.log(undefined >= 0); // false
-// console.log(undefined <= 0); // false
-
-
+console.log(undefined > 0); // false 
+console.log(undefined < 0); // false
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined <= 0); // false
